dec988d7fbe33858cdae047ac1e847b073cd1f45
Raff <113399703+RaffDv@users.noreply.github.com>
2023-03-11 09:52:33 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2F2F2",
+        "primary-light": "#F7F7F7",
+        secundary: "#EDB8FA",
+      },
+    },
+  },
+  plugins: [],
+};
